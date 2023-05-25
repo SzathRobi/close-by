2257990c6c15ba9c0ab7e73b8a134e09765d7029
robert SZATHMARI <robert.szathmari@shiwaforce.com>
2023-05-25 22:23:32 +0200

    fix types

--- pages/api/comments/index.js
+++ pages/api/comments/index.js
@@ -1,18 +1,6 @@
-import { getComments, postComments } from '../../../app/lib/mongo/comments';
+import { postComments } from '../../../app/lib/mongo/comments';
 
 const handler = async (request, response) => {
-	if (request.method === 'GET') {
-		try {
-			const { comments, error } = await getComments();
-
-			if (error) throw new Error(error);
-
-			return response.status(200).json({ comments });
-		} catch (error) {
-			return response.status(500).json({ error: error.message });
-		}
-	}
-
 	if (request.method === 'POST') {
 		try {
 			let bodyObject = JSON.parse(request.body);
