ec71b2ef3768fee833dfb08285cee57bf3899e2b
Szathmári Róbert <robert.szathmari@budapestkozut.hu>
2024-07-17 08:19:34 +0200

    fix: change env variable name

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,34 +1,34 @@
-import NextAuth from 'next-auth/next';
-import GoogleProvider from 'next-auth/providers/google';
+import NextAuth from "next-auth/next";
+import GoogleProvider from "next-auth/providers/google";
 
 export default NextAuth({
-	providers: [
-		GoogleProvider({
-			clientId: process.env.GOOGLE_ID ?? '',
-			clientSecret: process.env.GOOGLE_SECRET ?? '',
-			authorization: {
-				params: {
-					scope: 'email profile https://www.googleapis.com/auth/calendar'
-				}
-			}
-		})
-	],
-	secret: process.env.SECRET,
-	callbacks: {
-		async jwt({ token, user, account }) {
-			if (user) {
-				token.id = user.id;
-			}
-			if (account) {
-				token.accessToken = account.access_token;
-			}
-			return token;
-		},
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID ?? "",
+      clientSecret: process.env.GOOGLE_SECRET ?? "",
+      authorization: {
+        params: {
+          scope: "email profile https://www.googleapis.com/auth/calendar",
+        },
+      },
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async jwt({ token, user, account }) {
+      if (user) {
+        token.id = user.id;
+      }
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
 
-		async session({ session, token }: any) {
-			session.user.id = token.id;
-			session.accessToken = token.accessToken;
-			return session;
-		}
-	}
+    async session({ session, token }: any) {
+      session.user.id = token.id;
+      session.accessToken = token.accessToken;
+      return session;
+    },
+  },
 });
